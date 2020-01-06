e31d004401dacf44f28ce283c806b9dd2a39a98e
bate330 <bateu.email@yahoo.fr>
2020-01-06 17:14:03 +0100

    random name

---
+++ SmallerPrinter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class SmallerPrinter : IPrinter
+    {
+        public void Print(Account account)
+        {
+            Console.WriteLine("Dane konta: {0}", account.AccountNumber);
+            Console.WriteLine("Imię i nazwisko właściciela: {0}", account.GetFullName());
+            Console.WriteLine();
+        }
+    }
+}
